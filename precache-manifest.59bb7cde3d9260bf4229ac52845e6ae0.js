8aa294db3cdf224559c9cadd1c049fad739b8338
Alex Wu <alex606@att.net>
2022-11-20 15:15:09 -0500

    Updates

---
+++ precache-manifest.59bb7cde3d9260bf4229ac52845e6ae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dd5cbe0e2c8d8e04b76",
+    "url": "/christmas/static/css/main.296e4023.chunk.css"
+  },
+  {
+    "revision": "9dd5cbe0e2c8d8e04b76",
+    "url": "/christmas/static/js/main.9dd5cbe0.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/christmas/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/christmas/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "d0a75589cb684837dfa0",
+    "url": "/christmas/static/js/runtime~main.d0a75589.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/christmas/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3e995af0a7e5db40fddcbc880932a574",
+    "url": "/christmas/index.html"
+  }
+];
